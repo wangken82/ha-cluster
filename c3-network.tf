bb778846247acd7adda10ec50f74bd4f4d5bda1a
Kenneth Wang <mingchunwang@microsoft.com>
2022-07-04 10:51:05 +0800

    change all

--- c3-network.tf
+++ c3-network.tf
@@ -7,14 +7,14 @@ resource "azurerm_resource_group" "myrg" {
 }
 
 resource "azurerm_virtual_network" "myvnet" {
-  name                = "${var.prefix}-network"
+  name                = "${var.prefix}-vnet"
   resource_group_name =  azurerm_resource_group.myrg.name
   location            =  azurerm_resource_group.myrg.location
   address_space       = ["10.0.0.0/16"]
 }
 
 resource "azurerm_subnet" "mysubnet" {
-  name                 = "internal"
+  name                 = "cluster-subnet"
   virtual_network_name =  azurerm_virtual_network.myvnet.name
   resource_group_name  =  azurerm_resource_group.myrg.name
   address_prefixes     = [ "10.0.0.0/24" ]
