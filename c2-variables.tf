94ee84501dc377e9e68a5e49f082e5c73844cd05
Kenneth Wang <mingchunwang@microsoft.com>
2022-07-09 13:10:20 +0800

    9-Jul change

--- c2-variables.tf
+++ c2-variables.tf
@@ -19,6 +19,7 @@ variable "_version" {
   description = "Version of the image used to create VM, underscore added to avoid Terraform error"
 }
 
+#added by kenneth
 variable "cloudshell_public_ip" {
   description = "This IP added into ssh allow list for bastion VM"
   type = string
